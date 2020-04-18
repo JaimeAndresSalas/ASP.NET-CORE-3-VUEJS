7d5a0accb5e0e9ddd0fc97e44c9acd4e81b2439a
Jaime Andres Salas <jsalasmoreira@gmail.com>
2020-04-18 18:36:50 -0500

    Inicio del Web Api Project en .Net

---
+++ WebApiProject/WebApiProject/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProject.Controllers
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Product>> GetAll()
+        {
+            return Products;
+        }
+
+        private static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Id= 1,
+                Name= "Guitarra Electrica",
+                Price= 400,
+                Description= "Ideal Para aprender a tocar"
+            },
+            new Product
+            {
+                Id= 2,
+                Name = "Amplificador Guitarra",
+                Price= 1200,
+                Description = "Ideal para comenzar"
+            },
+        };
+    }
+}
