265eb91c19015cbf6cb60640b552ae3db9d1400c
Jaime Andres Salas <jsalasmoreira@gmail.com>
2020-04-18 23:03:23 -0500

    Creacion de Modelo SPA y carpeta src

---
+++ ProjectSpaSolution/src/Persistence.DataBase/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Persistence.DataBase
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
